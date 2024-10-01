d30d8fa4a8ef5253cde66fc105b1dd20f8592614
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 14:15:36 -0600

    [FEAT] Multiples cambios en el archivo BottomTabNavigator
    - importando createBottomTabNavigator  para crear el botón tab navigator
    - importando las vistas HomeScreen,ProfileScreen
    - creando ka variable Tab donde recibe la referencia de la función de createBottomTabNavigator
    - creando nuestras rutas Home y Profile para mostrar las vistas de HomeScreen y ProfileScreen

--- src/presentation/Navigators/BottomTabNavigator.tsx
+++ src/presentation/Navigators/BottomTabNavigator.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen,ProfileScreen } from '../screens';
+
+const Tab = createBottomTabNavigator();
 
 export const BottomTabNavigator = () => {
   return (
-    <View>
-      <Text>BottomTabNavigator</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   );
 };
