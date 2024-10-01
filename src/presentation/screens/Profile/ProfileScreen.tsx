e1dce037ad06e1550407fdf8e6a87013db0514fd
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 15:05:03 -0600

    [FEAT] Cambios en el archivo ProfileScreen
    - creamos la variable handleChangleProfile para leer del state la acción handleChangleProfile y porder cambiar el state
    - creamos otro botón pero ahora haremos referencia a handleChangleProfile  para cambiar el state pasando los mismos datos del botón anterior

--- src/presentation/screens/Profile/ProfileScreen.tsx
+++ src/presentation/screens/Profile/ProfileScreen.tsx
@@ -5,11 +5,16 @@ import { useProfileStore } from '../../../store/profile.store';
 export const ProfileScreen = () => {
     const name = useProfileStore(state => state.name);
     const email = useProfileStore(state => state.email);
+    const handleChangleProfile = useProfileStore(state => state.handleChangleProfile);
   return (
     <View>
       <Text>Hola, {name} con el email: {email}</Text>
 
       <Pressable onPress={()=> useProfileStore.setState({name:'name cambiado',email:'email cambiado'})}>
+        <Text>Change state</Text>
+      </Pressable>
+
+      <Pressable onPress={()=> handleChangleProfile('name cambiado','email cambiado') }>
         <Text>Change state</Text>
       </Pressable>
     </View>
