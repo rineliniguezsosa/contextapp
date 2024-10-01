773c67d88fe53ca63ac1c9d3aefaa53d0dff6da3
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 14:47:10 -0600

    [FEAT] Cambios en el archivo ProfileScreen
    - importamos el store useProfileStore para poder leer el state
    - creamos las variables name y email para leer del store el name y el email
    - establecemos el contenido del name y el email en el elemento Text para consumir el store

--- src/presentation/screens/Profile/ProfileScreen.tsx
+++ src/presentation/screens/Profile/ProfileScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View,Text } from 'react-native';
+import { useProfileStore } from '../../../store/profile.store';
 
 export const ProfileScreen = () => {
+    const name = useProfileStore(state => state.name);
+    const email = useProfileStore(state => state.email);
   return (
     <View>
-      <Text>ProfileScreen</Text>
+      <Text>Hola, {name} con el email: {email}</Text>
     </View>
   );
 };
