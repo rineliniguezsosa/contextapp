6313481b8f95bfd3bc4aba982bed5d54e63d2c68
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 14:01:57 -0600

    [FEAT] Creando el funcional component BottomTabNavigator

---
+++ src/presentation/Navigators/BottomTabNavigator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export const BottomTabNavigator = () => {
+  return (
+    <View>
+      <Text>BottomTabNavigator</Text>
+    </View>
+  );
+};
