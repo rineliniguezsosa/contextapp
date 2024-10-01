8538e4a7920dc17b695fa4bf9d58df6ea764838d
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 16:05:27 -0600

    [FEAT] Cambios en el archivo SettingScreen
    - deshabilitando la extrema dependencia de el hook useEffect
    - importando useEffect
    - importando useNavigation
    - creando la variable  navigation donde pasamos la referencia de la función de useNavigation
    - establecemos la función useEffect donde establecemos las configuraciones de la navegación para cambiar el title a "Count" y alineando el tilte al centro

--- src/presentation/screens/Settings/SettingScreen.tsx
+++ src/presentation/screens/Settings/SettingScreen.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React,{ useEffect } from 'react';
 import { View,Text,Pressable } from 'react-native';
 import { useSettingStore } from '../../../store/counter.store';
+import { useNavigation } from '@react-navigation/native';
 
 export const SettingScreen = () => {
     const count = useSettingStore(state => state.count);
     const handleIncrementBy = useSettingStore(state => state.handleIncrementBy);
+    const navigation = useNavigation();
+
+    useEffect(()=>{
+        navigation.setOptions({
+            title:'Count',
+            headerTitleAlign: 'center',
+        });
+    },[]);
+
   return (
     <View>
       <Text>SettingScreen</Text>
