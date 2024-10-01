8e09519f0dc205874a95d852158439b80c8f24d2
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 16:41:02 -0600

    [FEAT] Estableciendo estilos en el HomeScreen

--- src/presentation/screens/Home/HomeScreen.tsx
+++ src/presentation/screens/Home/HomeScreen.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View,Text } from 'react-native';
 import { useSettingStore } from '../../../store/counter.store';
+import { globalStyles } from '../../../styles/theme';
 
 export const HomeScreen = () => {
     const count = useSettingStore(state => state.count);
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>HomeScreen</Text>
 
       <Text>{count}</Text>
     </View>
