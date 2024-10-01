025beb5c338551cb1f87fbc8aad01841d41787a0
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 14:59:45 -0600

    [FEAT] Cambios en el store  useProfileStore
    - creando la prop  handleChangleProfile en la interface ProfileScreen donde devolveremos la referencia de una función donde establecimos los parametros name y email de tipo string, la función devuelve void vacio
    - creamos la función handleChangleProfil en el store useProfileStore donde devolveremos una función con las props name y email de tipo string y modificaremos los argumentos mediante el set función

--- src/store/profile.store.ts
+++ src/store/profile.store.ts
@@ -4,9 +4,13 @@ import { create } from 'zustand';
 export interface ProfileScreen {
     name:string,
     email:string,
+    handleChangleProfile:(name:string,email:string) => void
 }
 
 export const useProfileStore = create<ProfileScreen>()((set,get)=>({
     name:'rinel',
     email:'rineliniguezsosa@gmail.com',
+    handleChangleProfile:(name:string,email:string)=>{
+        set({name:name,email:email});
+    },
 }));
