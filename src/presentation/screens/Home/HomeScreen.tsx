9aa713212f7187fcb6e0dc1e1c4e893c121fead0
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 16:07:45 -0600

    [FEAT] Cambios en el archivo HomeScreen
    - leyendo el state del count del store  useSettingStore
    - estableciendo la counta en el elemento Text

--- src/presentation/screens/Home/HomeScreen.tsx
+++ src/presentation/screens/Home/HomeScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View,Text } from 'react-native';
+import { useSettingStore } from '../../../store/counter.store';
 
 export const HomeScreen = () => {
+    const count = useSettingStore(state => state.count);
   return (
     <View>
       <Text>HomeScreen</Text>
+
+      <Text>{count}</Text>
     </View>
   );
 };
