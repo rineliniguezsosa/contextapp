cff2b97ae27d2525cd672f241c397e2d26c2c32b
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 14:13:09 -0600

    [FEAT] Corrigiendo el nombre del funcional component por ProfileScreen me hizo falta una "e" en el nombre

--- src/presentation/screens/Profile/ProfileScreen.tsx
+++ src/presentation/screens/Profile/ProfileScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View,Text } from 'react-native';
 
-export const ProfileScren = () => {
+export const ProfileScreen = () => {
   return (
     <View>
       <Text>ProfileScreen</Text>
