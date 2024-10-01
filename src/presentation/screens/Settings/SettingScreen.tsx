72cd126597ca2ea06453068318e85f6b0c37a2e5
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 15:38:53 -0600

    [FEAT] Multiples cambios en el archivo  SettingScreen
    - importando el componente Pressable
    - importando el store Pressable
    - creando la variable count donde leeremos del state el count
    - creando la variable handleIncrementBy  para leer del store la referencia de la acción handleIncrementBy
    - definimos el componente Text para establecer la variable count
    - establecemos un simple botón con el texto Increment +1 donde pasamos la referencia del método handleIncrementB y pasamos el argumento 1 para incrementar la cuenta

--- src/presentation/screens/Settings/SettingScreen.tsx
+++ src/presentation/screens/Settings/SettingScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text,Pressable } from 'react-native';
+import { useSettingStore } from '../../../store/counter.store';
 
 export const SettingScreen = () => {
+    const count = useSettingStore(state => state.count);
+    const handleIncrementBy = useSettingStore(state => state.handleIncrementBy);
   return (
     <View>
       <Text>SettingScreen</Text>
+
+     <Text>{count}</Text>
+
+      <Pressable onPress={()=> handleIncrementBy(1) }>
+        <Text>Increment +1</Text>
+      </Pressable>
     </View>
   );
 };
