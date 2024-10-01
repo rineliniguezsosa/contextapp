454f98161d7e7b4a2e223ce110404fc050a16410
Rinel <rineliniguezsosa@gmail.com>
2024-10-01 16:42:22 -0600

    [FEAT] Estableciendo los estilos en el archivo ProfileScreen

--- src/presentation/screens/Profile/ProfileScreen.tsx
+++ src/presentation/screens/Profile/ProfileScreen.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { View,Text,Pressable } from 'react-native';
 import { useProfileStore } from '../../../store/profile.store';
+import { globalStyles } from '../../../styles/theme';
 
 export const ProfileScreen = () => {
     const name = useProfileStore(state => state.name);
     const email = useProfileStore(state => state.email);
     const handleChangleProfile = useProfileStore(state => state.handleChangleProfile);
   return (
-    <View>
-      <Text>Hola, {name} con el email: {email}</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>Hola, {name} con el email: {email}</Text>
 
-      <Pressable onPress={()=> useProfileStore.setState({name:'name cambiado',email:'email cambiado'})}>
+      <Pressable style={globalStyles.primaryButton} onPress={()=> useProfileStore.setState({name:'name cambiado',email:'email cambiado'})}>
         <Text>Change state</Text>
       </Pressable>
 
-      <Pressable onPress={()=> handleChangleProfile('name cambiado otra vez','email cambiado') }>
+      <Pressable style={globalStyles.primaryButton} onPress={()=> handleChangleProfile('name cambiado otra vez','email cambiado') }>
         <Text>Change state</Text>
       </Pressable>
     </View>
